49815deb96a5255cf7d7cbe71204ec626d636624
Cole Fuerth <colefuerth@gmail.com>
2022-12-04 00:32:01 -0500

    update template to use regex for input matching

--- template/AoC.py
+++ template/AoC.py
@@ -3,6 +3,7 @@
 import sys
 from itertools import product, count, permutations
 from copy import deepcopy
+import re
 
 # AoC template for python3
 
@@ -15,7 +16,12 @@ def part2(f:list) -> int:
 if __name__ == "__main__":
     # start by getting file as a list of strings
     fname = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
-    f = [l.split() for l in open(fname, 'r')]
+    regex = re.compile(r"(\d+)")
+    f = [
+        int(regex.findall(l)[0])
+        for l in open(fname, 'r').readlines()
+    ]
+    print(f)
 
     print("Part 1:", part1(deepcopy(f)))
     print("Part 2:", part2(deepcopy(f)))
