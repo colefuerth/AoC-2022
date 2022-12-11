682b31866fe0a82ebf133914d182cca7389611ec
Cole Fuerth <colefuerth@gmail.com>
2022-12-11 18:16:18 -0500

    update template

--- template/AoC.py
+++ template/AoC.py
@@ -5,6 +5,8 @@ from itertools import product, count, permutations
 from copy import deepcopy
 import re
 import numpy as np
+from collections import defaultdict, Counter
+import progressbar as pb
 
 
 def part1(f: list) -> int:
@@ -15,14 +17,14 @@ def part2(f: list) -> int:
     pass
 
 
-if __name__ == "__main__":
-    # start by getting file as a list of strings
-    fname = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+if __name__ == '__main__':
+    fname = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
     f = [
         l.strip()
         for l in open(fname, 'r').readlines()
+        if l.strip() != ''
     ]
     print(f)
 
-    print("Part 1:", part1(deepcopy(f)))
-    print("Part 2:", part2(deepcopy(f)))
+    print('Part 1:', part1(deepcopy(f)))
+    print('Part 2:', part2(deepcopy(f)))
