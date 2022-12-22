84a3cd8e1550173a56f94931695ac195d92259d4
Cole Fuerth <colefuerth@gmail.com>
2022-12-21 20:17:33 -0500

    template and requirements update

--- template/AoC.py
+++ template/AoC.py
@@ -4,20 +4,9 @@ import sys
 from itertools import product, count, permutations
 from copy import deepcopy
 import re
-import numpy as np
-from collections import defaultdict, Counter
-import progressbar as pb
 
 
-class Blueprint:
-    def __init__(self, ore:int, clay:int, obsidian:tuple, geode:tuple):
-        self.ore = ore
-        self.clay = clay
-        self.obsidian = obsidian
-        self.geode = geode
-
-
-def part1(blueprints: list) -> int:
+def part1(f: list) -> int:
     pass
 
 
@@ -32,13 +21,6 @@ if __name__ == '__main__':
         for l in open(fname, 'r').readlines()
         if l.strip() != ''
     ]
-    blueprints = []
-    for bp in re.split(r'Blueprint \d+: ', ' '.join(f)):
-        fd = list(map(int, re.findall(r'\d+', bp)))
-        ore, clay = fd[:2]
-        obsidian = tuple(fd[2:4])
-        geode = tuple(fd[4:6])
-        blueprints.append(Blueprint(ore, clay, obsidian, geode))
 
-    print('Part 1:', part1(blueprints))
-    print('Part 2:', part2(blueprints))
+    print('Part 1:', part1(f))
+    print('Part 2:', part2(f))
