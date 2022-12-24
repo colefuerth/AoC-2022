72b24d641e3f84615d30948974d7f759ad42d993
Cole Fuerth <colefuerth@gmail.com>
2022-12-24 09:59:17 -0500

    day 23 part 1

--- day23/AoC.py
+++ day23/AoC.py
@@ -1,26 +1,105 @@
 # /usr/bin/python3
 
 import sys
-from itertools import product, count, permutations
+from itertools import product, count
 from copy import deepcopy
-import re
+
+# (x, y) = (row, col)
+
+ADJ = list(product([-1, 0, 1], repeat=2))
+ADJ.remove((0, 0))
+
+DIR = [
+    [(-1, 0), [(-1, 0), (-1, 1), (-1, -1)]],  # N
+    [(1, 0), [(1, 0), (1, 1), (1, -1)]],  # S
+    [(0, -1), [(0, -1), (1, -1), (-1, -1)]],  # W
+    [(0, 1), [(0, 1), (1, 1), (-1, 1)]],  # E
+]
 
 
-def part1(f: list) -> int:
-    pass
+def add_tup(a, b) -> tuple:
+    return a[0] + b[0], a[1] + b[1]
 
 
-def part2(f: list) -> int:
-    pass
+def print_elves(elves: set):
+    xmin = min(elves, key=lambda x: x[0])[0]
+    xmax = max(elves, key=lambda x: x[0])[0]
+    ymin = min(elves, key=lambda x: x[1])[1]
+    ymax = max(elves, key=lambda x: x[1])[1]
+    for i in range(xmin, xmax + 1):
+        for j in range(ymin, ymax + 1):
+            if (i, j) in elves:
+                print('#', end='')
+            else:
+                print('.', end='')
+        print()
+
+
+def part1(elves: set) -> int:
+
+    for _ in range(10):
+        # first half of round, find proposals
+        proposals = {}
+        for elf in elves:
+            if any(add_tup(elf, a) in elves for a in ADJ):
+                for d, fan in DIR:
+                    if not any(add_tup(elf, dd) in elves for dd in fan):
+                        proposals[elf] = add_tup(elf, d)
+                        break
+        # second half, apply proposals if no collision
+        plist = list(proposals.values())
+        proposals = {k: v for k, v in proposals.items() if plist.count(v) == 1}
+        for elf, newelf in proposals.items():
+            elves.remove(elf)
+            elves.add(newelf)
+        DIR.append(DIR.pop(0))
+
+    xmin = min(elves, key=lambda x: x[0])[0]
+    xmax = max(elves, key=lambda x: x[0])[0]
+    ymin = min(elves, key=lambda x: x[1])[1]
+    ymax = max(elves, key=lambda x: x[1])[1]
+    rectangle = (xmax - xmin + 1) * (ymax - ymin + 1)
+    return rectangle - len(elves)
+
+
+def part2(elves: set) -> int:
+    c = count()
+    for _ in c:
+        # first half of round, find proposals
+        proposals = {}
+        for elf in elves:
+            if any(add_tup(elf, a) in elves for a in ADJ):
+                for d, fan in DIR:
+                    if all(add_tup(elf, df) not in elves for df in fan):
+                        proposals[elf] = add_tup(elf, d)
+                        break
+        # second half, apply proposals if no collision
+        plist = list(proposals.values())
+        proposals = {k: v for k, v in proposals.items() if plist.count(v) == 1}
+        if len(proposals) == 0:
+            print(f'After {_ + 1} rounds:')
+            print_elves(elves)
+            return next(c)
+        for elf, newelf in proposals.items():
+            elves.remove(elf)
+            elves.add(newelf)
+        # rotate
+        print(f'After {_ + 1} rounds:')
+        print(proposals)
+        print_elves(elves)
+        DIR.append(DIR.pop(0))
+
+    return next(c)
 
 
 if __name__ == '__main__':
     fname = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
-    f = [
-        l.strip()
-        for l in open(fname, 'r').readlines()
-        if l.strip() != ''
-    ]
 
-    print('Part 1:', part1(f))
-    print('Part 2:', part2(f))
+    elves = set()
+    for i, l in enumerate(open(fname, 'r').readlines()):
+        for j, c in enumerate(l.strip()):
+            if c == '#':
+                elves.add((i, j))
+
+    print('Part 1:', part1(deepcopy(elves)))
+    print('Part 2:', part2(deepcopy(elves)))
